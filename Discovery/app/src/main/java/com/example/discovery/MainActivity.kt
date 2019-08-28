5244f957ed7fafbe66603aec14c5c6649f5ffd56
Cristian Camilo Novoa Avellaneda <cnovoa@belatrix.com>
2019-08-28 16:35:30 -0500

    working on asynktask

--- Discovery/app/src/main/java/com/example/discovery/MainActivity.kt
+++ Discovery/app/src/main/java/com/example/discovery/MainActivity.kt
@@ -1,15 +1,26 @@
 package com.example.discovery
 
 
+import android.app.Dialog
+import android.app.ProgressDialog
 import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothDevice
+import android.content.Context
 import android.content.Intent
+import android.net.ConnectivityManager
+import android.os.AsyncTask
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.Settings
 import android.widget.Button
+import android.widget.ProgressBar
 import android.widget.TextView
 import android.widget.Toast
 import com.beust.klaxon.JsonObject
 import com.beust.klaxon.Parser
+import kotlinx.android.synthetic.main.activity_main.*
+import okhttp3.OkHttpClient
+import okhttp3.Request
 import java.net.URL
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
@@ -17,23 +28,27 @@ import org.jetbrains.anko.doAsync
 import org.jetbrains.anko.uiThread
 import java.lang.Exception
 import java.lang.StringBuilder
+import java.net.NetworkInterface
+import java.util.*
 
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var context: Context
 
-    @ExperimentalStdlibApi
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
 
-        val txtView = findViewById<TextView>(R.id.textView)
         val btnOn = findViewById<Button>(R.id.buttonOnOff)
         val btnDisc = findViewById<Button>(R.id.buttonDisc)
         val btnList = findViewById<Button>(R.id.buttonList)
+        val txtViewResult = findViewById<TextView>(R.id.textViewExist)
         val adapter = BluetoothAdapter.getDefaultAdapter()
+
+        context = this
 
         btnList.setOnClickListener {
             val activityIntent = Intent(this, StudentListActivity::class.java)
@@ -59,39 +74,68 @@ class MainActivity : AppCompatActivity() {
                 //Toast.makeText(applicationContext, "Discovering", Toast.LENGTH_SHORT).show()
             }
 
-            val current = LocalDateTime.now().minusMonths(1)
-            val formatter = DateTimeFormatter.ofPattern("d-M-yyyy")
-            val formatted = current.format(formatter)
-            val today = LocalDateTime.now().format(formatter)
-
-            val link =
-                "https://firestore.googleapis.com/v1/projects/attendancelistapp/databases/(default)/documents/attendance/$formatted/students/201513389"
-
-            //Para probar que funciona el día que se tomó la asistencia
-            //val link = "https://firestore.googleapis.com/v1/projects/attendancelistapp/databases/(default)/documents/attendance/5-7-2019/students/201513389"
+            getStudentList().execute()
+        }
+    }
 
 
-            doAsync {
-                var code = ""
-                try {
-                    val data = URL(link).readText()
-                    val parser: Parser = Parser.default()
-                    val stringBuilder: StringBuilder = StringBuilder(data)
-                    val json: JsonObject = parser.parse(stringBuilder) as JsonObject
-                    code = "${json.string("name")}"
-                } catch (e: Exception){
-                    println("Not found")
-                    code=""
-                }
-                uiThread {
-                    if (code.contains("201513389"))
-                        //Siempre se esta suponiendo que se ejecuta el mismo día que se llama a lista
-                        txtView.setText("Attended on: $today")
-                    if(code.equals(""))
-                        txtView.setText("No results")
-                }
+    internal inner class getStudentList : AsyncTask<Void, Void, String>() {
+        lateinit var progressDialog: Dialog
+        var internet = false
 
+        override fun onPreExecute() {
+            super.onPreExecute()
+            progressDialog = ProgressDialog(context)
+            progressDialog.setCancelable(false)
+            progressDialog.setTitle("Searching MAC on today's attendace")
+            progressDialog.show()
+
+        }
+
+        override fun doInBackground(vararg params: Void?): String {
+            if (isNetworkAvailable()) {
+                internet = true
+                val client = OkHttpClient()
+                val current = LocalDateTime.now().minusMonths(1)
+                val formatter = DateTimeFormatter.ofPattern("d-M-yyyy")
+                val formatted = current.format(formatter)
+                val today = LocalDateTime.now().format(formatter)
+
+                //val link = "https://firestore.googleapis.com/v1/projects/attendancelistapp/databases/(default)/documents/attendance/5-7-2019/students/201513389"
+                val link = "https://firestore.googleapis.com/v1/projects/attendancelistapp/databases/(default)/documents/attendance/$formatted/students/201513389"
+
+                val request = Request.Builder().url(link).build()
+                val response = client.newCall(request).execute()
+                return response.body?.string().toString()
+
+
+            } else {
+                return ""
             }
         }
+
+        override fun onPostExecute(result: String?) {
+            super.onPostExecute(result)
+            progressDialog.dismiss()
+
+            //textViewExist.text = result.toString()
+
+
+            if (result.toString().contains("F4:60:E2:F3:C1:6F")) {
+                //Siempre se esta suponiendo que se ejecuta el mismo día que se llama a lista
+                textViewExist.text = "The MAC F4:60:E2:F3:C1:6F attended today"
+            } else {
+                textViewExist.text ="No results"
+            }
+
+        }
+
+        private fun isNetworkAvailable(): Boolean {
+            val connectivityManager =
+                getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+            val activeNetworkInfo = connectivityManager.activeNetworkInfo
+            return activeNetworkInfo != null && activeNetworkInfo.isConnected
+        }
+
     }
 }
--- Discovery/app/src/main/java/com/example/discovery/StudentListActivity.kt
+++ Discovery/app/src/main/java/com/example/discovery/StudentListActivity.kt
@@ -1,8 +1,14 @@
 package com.example.discovery
 
+import android.app.DatePickerDialog
+import android.os.AsyncTask
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
+import android.widget.TextView
+import java.net.URL
+import java.util.*
 
 class StudentListActivity : AppCompatActivity() {
 
@@ -11,5 +17,41 @@ class StudentListActivity : AppCompatActivity() {
         setContentView(R.layout.activity_student_list)
 
         val btnDate = findViewById<Button>(R.id.buttonDate)
+        val textViewDate = findViewById<TextView>(R.id.textViewDate)
+
+        val c = Calendar.getInstance()
+        val year = c.get(Calendar.YEAR)
+        val month = c.get(Calendar.MONTH)
+        val day = c.get(Calendar.DAY_OF_MONTH)
+
+         btnDate.setOnClickListener{
+             val dpd = DatePickerDialog(this, DatePickerDialog.OnDateSetListener{ view, nyear, nmonth, ndayOfMonth ->
+                 //Log.d("ESTA ES LA URL","https://firestore.googleapis.com/v1/projects/attendancelistapp/databases/(default)/documents/attendance/"+ndayOfMonth + "-" + nmonth + "-" + nyear+"/students?pageSize=50")
+                 getJsonURL("https://firestore.googleapis.com/v1/projects/attendancelistapp/databases/(default)/documents/attendance/"+ndayOfMonth + "-" + nmonth + "-" + nyear+"/students?pageSize=50")
+                 var monthView = month+1
+                 textViewDate.setText(""+ ndayOfMonth + "/" + monthView + "/" + nyear)
+             }, year, month, day)
+
+             dpd.show()
+         }
+    }
+
+    internal inner class compareStudents : AsyncTask<Void, Void, String> (){
+        override fun onPreExecute() {
+            super.onPreExecute()
+        }
+        override fun doInBackground(vararg params: Void?): String {
+            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        }
+
+        override fun onPostExecute(result: String?) {
+            super.onPostExecute(result)
+        }
+
+    }
+
+    private fun getJsonURL(url: String): String {
+        return URL(url).readText()
+
     }
 }
